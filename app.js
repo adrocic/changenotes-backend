4adc3c01cc4b86fb96c95a781434d449a71fa1d9
Anel <anel.drocic@gmail.com>
2023-12-30 00:48:46 -0600

    updated cron job, updated mongo connections, updated retrieval and storage of collection

--- app.js
+++ app.js
@@ -1,94 +1,114 @@
-require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
-const { OpenAIApi } = require("openai");
+const OpenAiAPI = require("openai");
 const MongoClient = require("mongodb").MongoClient;
 const cron = require("node-cron");
 
-// Initialize Express app
+// Configure the OpenAI API client
+const openai = new OpenAiAPI({
+  apiKey: process.env.OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
-const openai = new OpenAIApi({ key: OPEN_AI_KEY });
-const mongoUrl = `mongodb+srv://${DB_USER}:${DB_PASSSWORD}@cluster0.fn2ukws.mongodb.net/?retryWrites=true&w=majority`;
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017";
 
-// Connect to MongoDB
-MongoClient.connect(
-  mongoUrl,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    if (err) {
-      console.error("Error connecting to MongoDB:", err);
-      process.exit(1);
-    }
+let db; // Define db at a higher scope
 
-    const db = client.db("changelogs");
+// Function to establish MongoDB connection
+async function connectToMongo() {
+  try {
+    const client = await MongoClient.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = client.db("changelogs");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+  }
+}
 
-    // Create the "changelogs" collection (if it doesn't exist)
-    db.createCollection("changelogs", (err, collection) => {
-      if (err) {
-        console.error("Error creating collection:", err);
-        process.exit(1);
+const fetchAndSummarizeChangelogs = async () => {
+  try {
+    // Fetch the changelogs from the React repository on GitHub
+    const githubResponse = await axios.get(
+      "https://api.github.com/repos/facebook/react/contents/CHANGELOG.md"
+    );
+
+    if (githubResponse.status === 200) {
+      const changelogContent = Buffer.from(
+        githubResponse.data.content,
+        "base64"
+      ).toString("utf-8");
+
+      // Split the changelog into smaller chunks if it's too large
+      const chunkSize = 1000; // Adjust as needed
+      const chunks = [];
+
+      for (let i = 0; i < changelogContent.length; i += chunkSize) {
+        chunks.push(changelogContent.slice(i, i + chunkSize));
       }
 
-      console.log("Collection created:", collection.collectionName);
+      // Process and summarize each chunk
+      for (const chunk of chunks) {
+        const prompt = `Summarize the React changelog:\n${chunk}`;
+        const response = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          prompt: prompt,
+          maxTokens: 100,
+        });
+        if (
+          response &&
+          response.choices &&
+          response.choices[0] &&
+          response.choices[0].text
+        ) {
+          const summarizedChangelog = response.choices[0].text;
+          console.log(summarizedChangelog);
 
-      // Define a function to fetch and summarize changelogs
-      const fetchAndSummarizeChangelogs = async () => {
-        try {
-          // Fetch the changelogs from the React repository on GitHub
-          const githubResponse = await axios.get(
-            "https://api.github.com/repos/facebook/react/contents/CHANGELOG.md"
-          );
+          // Check if the changelog is current (e.g., based on a date)
+          const currentDate = new Date();
+          const isChangelogCurrent = true; // Implement your logic to check if it's current
 
-          if (githubResponse.status === 200) {
-            const changelogContent = Buffer.from(
-              githubResponse.data.content,
-              "base64"
-            ).toString("utf-8");
-
-            // Use OpenAI to summarize the changelog data
-            const prompt = `Summarize the React changelog:\n${changelogContent}`;
-            const response = await openai.createCompletion({
-              prompt,
-              max_tokens: 100, // Adjust for desired summary length
+          if (isChangelogCurrent) {
+            // Store the summarized changelog in MongoDB
+            await db.collection("changelogs").insertOne({
+              summary: summarizedChangelog,
+              timestamp: currentDate,
             });
-
-            if (
-              response &&
-              response.choices &&
-              response.choices[0] &&
-              response.choices[0].text
-            ) {
-              const summarizedChangelog = response.choices[0].text;
-
-              // Check if the changelog is current (e.g., based on a date)
-              const currentDate = new Date();
-              const isChangelogCurrent = true; // Implement your logic to check if it's current
-
-              if (isChangelogCurrent) {
-                // Store the summarized changelog in MongoDB
-                db.collection("changelogs").insertOne({
-                  summary: summarizedChangelog,
-                  timestamp: currentDate,
-                });
-              }
-            }
           }
-        } catch (error) {
-          console.error("Error fetching or summarizing changelogs:", error);
         }
-      };
-
-      // Schedule the cron job to run every 20 minutes
-      cron.schedule("*/20 * * * *", () => {
-        console.log("Running the cron job...");
-        fetchAndSummarizeChangelogs();
-      });
-    });
+      }
+    }
+  } catch (error) {
+    console.error("Error fetching or summarizing changelogs:", error);
   }
-);
+};
 
+connectToMongo().then(() => {
+  // Schedule the cron job to run every 20 minutes
+  cron.schedule("*/20 * * * *", () => {
+    console.log("Running the cron job...");
+    fetchAndSummarizeChangelogs();
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Define API route to retrieve the latest changelog
 app.get("/api/retrieve-changelog", (req, res) => {
+  fetchAndSummarizeChangelogs();
+  if (!db) {
+    return res
+      .status(500)
+      .json({ error: "Database connection not established" });
+  }
+
   // Connect to the MongoDB collection where the changelog data is stored
   const changelogCollection = db.collection("changelogs");
 
@@ -110,8 +130,4 @@ app.get("/api/retrieve-changelog", (req, res) => {
         return res.status(404).json({ error: "Changelog not found" });
       }
     });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
